60e83cab6b6dc10c51f9c83eed266efaaeb53781
Nandithadaggupati <nandithadaggupati888@gmail.com>
2026-01-17 00:29:34 +0530

    Add terraform folder and contents

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,29 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.100"
     }
   }
 }
 
 provider "aws" {
-  region                      = var.region
+  region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+  s3_use_path_style           = true
 
   endpoints {
-    s3 = var.localstack_s3_endpoint
+    s3 = "http://localhost:4566"
   }
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "etl-demo-bucket"
 }
 
 resource "aws_s3_bucket" "raw" {
@@ -32,4 +36,14 @@ resource "aws_s3_bucket_public_access_block" "raw_block" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "raw_csv" {
+  bucket = aws_s3_bucket.raw.id
+  key    = "raw/raw_data.csv"
+  source = "${path.module}/../raw_data.csv"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.raw.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,1 @@
-variable "bucket_name" {
-  type        = string
-  description = "S3 bucket name"
-  default     = "etl-demo-bucket"
-}
 
-variable "region" {
-  type        = string
-  description = "AWS region"
-  default     = "us-east-1"
-}
-
-variable "localstack_s3_endpoint" {
-  type        = string
-  description = "LocalStack S3 endpoint"
-  default     = "http://localstack:4566"
-}
